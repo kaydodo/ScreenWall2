49424efc8302e74746733fc5fec99f6be64c02ef
kaydodo <ah_do@163.com>
2026-05-17 19:57:39 +0800

    添加v37版本 - Qt5摄像头弹窗自动点击

--- mumu_camera_hook/injector.cpp
+++ mumu_camera_hook/injector.cpp
@@ -88,7 +88,7 @@ int main(int argc, char* argv[]) {
     GetModuleFileNameA(NULL, dllPath, MAX_PATH);
     char* lastSlash = strrchr(dllPath, '\\');
     if (lastSlash) {
-        strcpy(lastSlash + 1, "camera_hook36.dll");
+        strcpy(lastSlash + 1, "camera_hook37.dll");
     }
     
     printf("=== MuMu Camera Hook Injector ===\n");
