bccccaa9d5b5c65029d28927c445479a1bb77a36
kaydodo <ah_do@163.com>
2026-05-22 23:33:42 +0800

    修改MUMU超时离线日志为 模拟器已超时断开

--- server/server.js
+++ server/server.js
@@ -4501,7 +4501,7 @@ setInterval(() => {
       lastPushTime.delete(id); // 清理推送时间追踪
       changed = true;
       if (id === 'MUMU-service') {
-         serverLog(`[MUMU] 模拟器微服务超时离线`);
+         serverLog(`[MUMU] 模拟器已超时断开`);
        } else {
         serverLog(`[!] 超时离线: ${dev.deviceName}`);
       }
