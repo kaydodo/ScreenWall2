fcd433ab427862eaff1ef48618b80a349c4a2c99
kaydodo <ah_do@163.com>
2026-05-28 01:48:44 +0800

    Add: Reverse proxy for NAS - /NAS/ path proxies to http://127.0.0.1:5244

---
+++ alist-reset-password.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ========================================
+echo      Alist Password Reset
+echo ========================================
+echo.
+echo This will reset the admin password
+echo.
+cd /d "D:\alist"
+alist.exe admin random
+echo.
+echo ========================================
+echo.
+pause
---
+++ alist-start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ========================================
+echo      Alist NAS Quick Start Script
+echo ========================================
+echo.
+echo Starting Alist...
+echo.
+echo Access URL: http://127.0.0.1:5244
+echo.
+echo ========================================
+echo.
+cd /d "D:\alist"
+alist.exe server
