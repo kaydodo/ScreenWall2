d5b556a81ff519579cd7ba64168606ca45dd06e9
kaydodo <ah_do@163.com>
2026-05-29 00:50:37 +0800

    fix: 修复网关代理Location重定向循环
    避免对已包含路由前缀的Location重复添加

--- server/server.js
+++ server/server.js
@@ -275,7 +275,9 @@ proxy.on('proxyRes', (proxyRes, req, res) => {
   if (route && proxyRes.headers['location']) {
     let location = proxyRes.headers['location'];
     if (location.startsWith('/') && !location.startsWith('//')) {
-      proxyRes.headers['location'] = route + location;
+      if (!location.startsWith(route + '/') && location !== route) {
+        proxyRes.headers['location'] = route + location;
+      }
     }
   }
 
